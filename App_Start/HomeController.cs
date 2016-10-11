e279d8568725773bb14b6feadac00bac390af2a1
yetinca <s.dziew@gmail.com>
2016-10-11 13:27:55 +0200

    initial

---
+++ App_Start/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testowanie.App_Start
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        // GET: Home
+        public ActionResult Index()
+        {
+            var dataContext = new BazaTestEntities();
+            var osoby = dataContext.osoba.ToList();
+            string lista = "";
+          foreach (var n in dataContext.osoba){
+                lista += n;
+            }
+            Console.WriteLine(lista);
+            return View(osoby);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ scripts/MainController.js
@@ -0,0 +1,7 @@
+﻿
+var MainController = function ($scope, $http) {
+    $http.get("http://localhost:50723/home")
+             .success(function (response) {
+                 $scope.names = records;
+             });
+});
